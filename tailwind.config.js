1c971032ae17823e6946bc836306a6e0f1590113
aleem_junaid <abdul_aleem@ragzon.com>
2023-08-24 15:43:40 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: "#12D3BF",
+        mainTextColor: "#333333",
+        lightGrayColor: "#999999",
+        lightGreenColor: "#e7faf8",
+        lightRedColor: "#FF6666",
+        hrColor: "#CCCCCC",
+        darkBlueColor: "#212529",
+        borderColor: "#A2A2A3",
+        blueButtonColor: "#007BFF",
+        footerBgColor: "#292933",
+        footerTextColor: "#808080",
+      },
+    },
+  },
+  plugins: [],
+};
